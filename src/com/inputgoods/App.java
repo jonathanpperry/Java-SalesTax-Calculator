bb6b2450fab3f672fce8e4beb82dc1e2173bef93
Jonny Ramen <jpper@umich.edu>
2019-10-07 14:44:55 +0900

    Added text fields for sales tax

--- src/com/inputgoods/App.java
+++ src/com/inputgoods/App.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 public class App {
     private JButton submitButton;
+    private JPanel panelMain;
     private JTextArea textArea1;
     private JTextArea textArea2;
 
@@ -13,8 +14,17 @@ public class App {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                JOptionPane.showMessageDialog(null, "Hello world!");
             }
         });
     }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
