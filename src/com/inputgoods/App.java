d0d484774942703a3718f33210e0fcbc76df8147
Jonny Ramen <jpper@umich.edu>
2019-10-07 14:30:36 +0900

    Added layout of GUI for sales tax I/O

---
+++ src/com/inputgoods/App.java
@@ -0,0 +1,20 @@
+package com.inputgoods;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton submitButton;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+
+    public App() {
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
